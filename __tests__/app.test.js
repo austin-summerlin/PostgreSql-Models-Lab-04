c5d67e0391ef2c033b908ae70a11039c77c3bd82
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-10 17:26:51 -0700

    first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,3 +8,30 @@ describe('demo routes', () => {
     return setup(pool);
   });
 });
+
+// CRUD
+// C - create POST   INSERT
+// R - read   GET    SELECT
+// U - update PUT    UPDATE
+// D - delete DELETE DELETE
+
+describe('dog routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a dog via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/dogs')
+      .send({ name: 'spot', age: 5, type: 'pitbull' });
+
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'spot',
+      age: 5,
+      type: 'pitbull'
+    });
+  });
+});
+
+
