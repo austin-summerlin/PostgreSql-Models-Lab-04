9192b84c3003c205866a685ced157b169bb74e0b
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-14 09:01:54 -0700

    remove rogue comma

--- __tests__/app-people.test.js
+++ __tests__/app-people.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import People from '../lib/models/People.js';
 
 // CRUD
 // C - create POST   INSERT
@@ -25,5 +26,15 @@ describe('people routes', () => {
       born: '27 Dec 1901',
       died: '06 May 1992'
     });
+  });
+
+  it('finds a person by id via GET', async () => {
+    const people = await People.insert({
+      name: 'Marlene Dietrich',
+      born: '27 Dec 1901',
+      died: '06 May 1992'
+    });
+    const res = await request(app).get(`/api/v1/people/${people.id}`);
+    expect(res.body).toEqual(people);
   });
 });
